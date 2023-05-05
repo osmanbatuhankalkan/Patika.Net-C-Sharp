44120b55738369a1eb72828fff036853844dfda1
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-05-05 15:45:26 +0300

    Add files via upload

---
+++ Donguler-for-loop-break-continue/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ekrandan girilen sayıya kadar olan tek sayılarını ekrana yazdır.
+            Console.Write("Lütfen bir sayı Giriniz: ");
+            int sayac= int.Parse(Console.ReadLine());
+            
+            ///Console Readline string olarak algılar o yüzde integer a çevirmek lazım "Parse" kullanıyoruz
+            for (int i =1; i<sayac; i++)
+            {
+                if(i%2 == 1)//Komutlar
+                    Console.WriteLine(i);
+            }    
+            
+            // 1 ile 1000 arasındaki tek ve çift sayıların ken içinde toplamlarını ekrana yazdır
+            //daha sonra erişebilmek için for dışında tanımlama yapmamız gerekiyor
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i =1; i<=1000; i++)
+
+            
+            {
+                if(i%2 ==1)
+                    tekToplam += i; //tekToplam = tekToplam +i; demek
+                else
+                    ciftToplam +=i;
+                
+                Console.WriteLine("Tek Toplam" + tekToplam);
+                Console.WriteLine("Çift Toplam" + ciftToplam);
+                
+                //break, continue
+                ///döngü sonlandırma, bir şarata bağlı olarak mevcut döngüde atlama yapmak istiyorsanız.
+
+                for (int o = 1; o < 10; o++)
+                {
+                    if(o==4)
+                        break;
+                    Console.WriteLine(o);
+                }
+                for (int o = 1; o < 10; o++)
+                {
+                    if(o==4)
+                        continue;
+                    Console.WriteLine(o);
+                }
+                
+            }
+        }
+    }
+}
---
+++ While_ve_ForEach_Do-ngu-leri/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace While_ve_ForEach_Döngüleri
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            //While
+            //1 den baslayarak consoldan girilen sayıya kadar (sayı dahil) ortalam hesaplayıp consola yazıralım
+            Console.WriteLine("Bir sayı giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            //ortalama almak için toplama ihtiyac var 
+            int toplama = 0;
+            while(sayac <= sayi)
+            {
+                toplama+= sayac;
+                sayac ++;
+            }
+            Console.WriteLine(toplama/sayi);
+
+            // a'dan z'ye kadar tüm harfleri console çıkar
+            char karakter = 'a';
+            while (karakter < 'z')
+            {
+                Console.WriteLine(karakter);
+                karakter++;
+            }
+            Console.WriteLine("********FOREACH*********");
+            string[] arabalar ={"BMW","FORD","VS"};
+            //Var tipi belli olmayan genel bir tanım ifadesi için böyle düşünebiliriz
+            //item(araba)her bi degerimiz için bmw for vs 
+            //collection(arabalar)
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+            
+        }
+    }
+}

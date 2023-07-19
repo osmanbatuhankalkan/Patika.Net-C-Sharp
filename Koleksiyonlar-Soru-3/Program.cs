ec7ab2053d39ff82fa50e2f05440d284bda68df1
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-07-19 23:40:13 +0300

    Add files via upload

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kolleksiyon_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> harfler = new List<char>();//liste içinde arama yapabilmek için LİST<T> tipini seçtim
+            string sesliHarfler = "a,e,ı,i,u,ü,o,ö";//harflerimizi tanımladık
+
+            System.Console.WriteLine("Lütfen Cümlenizi giriniz");
+            string cümle = System.Console.ReadLine();
+            foreach (var item in cümle)
+            {
+                if(sesliHarfler.Contains(item))//contains'imiz liste içinde arama yapar
+                {
+                    harfler.Add(item);
+                }
+            }
+
+            harfler.Sort();//sıralayalım
+            harfler.ForEach(sıralama =>System.Console.WriteLine(sıralama));
+
+           
+        }
+    }
+}
